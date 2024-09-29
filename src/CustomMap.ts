d3b8d7ecf3984d1b9be4a7c2a6dde0d1a719532a
Abdelrahman435 <ah0383723@gmail.com>
2024-09-29 20:56:10 +0300

    Maps

---
+++ src/CustomMap.ts
@@ -0,0 +1,46 @@
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    // when we use  or oprator typeScript then compare between the two classes we added and when we try to use mappable we see the attributes and methods that are same as locations
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
